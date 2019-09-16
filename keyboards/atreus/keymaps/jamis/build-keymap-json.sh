77b0a35b991cf6f20b23ec08196ff1f7e6d67c86
James Hagerman <james.hagerman@gmail.com>
2019-09-16 16:01:16 -0700

    Adding a maddening script to build a keymap.json from the keymap.c

---
+++ keyboards/atreus/keymaps/jamis/build-keymap-json.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# This is a hack.
+# ./build-keymap-json.sh | node > keymap.json
+BEFORE_TOO_MUCH=`cat keymap.c | grep -v include | gcc -E -P - | awk 'NF' | grep -zPo '(?s)PROGMEM keymaps.*?};' | head -n -1 | tail -n +2 | sed -r 's/\s+//g' | sed -z 's/,\n\([^[]\)/,\1/g'`
+
+MOSTLY_THERE=`echo "$BEFORE_TOO_MUCH" | sed 's/\[/layers\[/g' | sed 's/=LAYOUT(/ = \[/' | sed -z 's/),\n/\],\n/g' | sed -z 's/"\n/"/g' | sed -z 's/\n)/\n\]/g'`
+
+FINALLY=`echo "$MOSTLY_THERE" | sed -z 's/\[\n/\["/g' | sed -z 's/\n\],\n/"\]\n/g' | sed -z 's/\n\]\n/\"]\n/g' | sed 's/,/","/g'`
+
+echo "let layers = []"
+echo "$FINALLY"
+echo "let keymap = {'keyboard':'atreus', 'keymap':'jamis-atreus', 'author':'jamis', 'notes':'', 'layout':'LAYOUT', layers}"
+echo "console.log(JSON.stringify(keymap))"
+
+
