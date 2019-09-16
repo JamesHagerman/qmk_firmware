f036797d205d9af63196d8e59072ca09b96c1251
James Hagerman <james.hagerman@gmail.com>
2019-09-16 16:15:03 -0700

    fixing a bug

--- keyboards/atreus/keymaps/jamis/build-keymap-json.sh
+++ keyboards/atreus/keymaps/jamis/build-keymap-json.sh
@@ -5,7 +5,9 @@ BEFORE_TOO_MUCH=`cat keymap.c | grep -v include | gcc -E -P - | awk 'NF' | grep 
 
 MOSTLY_THERE=`echo "$BEFORE_TOO_MUCH" | sed 's/\[/layers\[/g' | sed 's/=LAYOUT(/ = \[/' | sed -z 's/),\n/\],\n/g' | sed -z 's/"\n/"/g' | sed -z 's/\n)/\n\]/g'`
 
-FINALLY=`echo "$MOSTLY_THERE" | sed -z 's/\[\n/\["/g' | sed -z 's/\n\],\n/"\]\n/g' | sed -z 's/\n\]\n/\"]\n/g' | sed 's/,/","/g'`
+FIX_LT=`echo "$MOSTLY_THERE" | sed -z 's/\[\n/\["/g' | sed -z 's/\n\],\n/"\]\n/g' | sed -z 's/\n\]\n/\"]\n/g' | sed 's/,/","/g'`
+
+FINALLY=`echo "$FIX_LT" | sed 's/\(LT([[:digit:]]\)","/\1,/g'`
 
 echo "let layers = []"
 echo "$FINALLY"
