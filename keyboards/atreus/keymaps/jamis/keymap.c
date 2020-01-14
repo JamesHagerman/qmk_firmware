3fcfaace23b4e566a037863abf8275be74391de3
James Hagerman <james.hagerman@gmail.com>
2020-01-14 01:28:05 -0800

    Replacing duplicate with a more useful key

--- keyboards/atreus/keymaps/jamis/keymap.c
+++ keyboards/atreus/keymaps/jamis/keymap.c
@@ -74,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_QUOT, KC_COMM, KC_DOT,  KC_P,    KC_Y,                      KC_F,    KC_G,    KC_C,    KC_R,    KC_L,
     KC_A,    KC_O,    KC_E,    KC_U,    KC_I,                      KC_D,    KC_H,    KC_T,    KC_N,    KC_S,
     KC_SCLN, KC_Q,    KC_J,    KC_K,    KC_X,                      KC_B,    KC_M,    KC_W,    KC_V,    KC_Z,
-    KC_ESC, KC_LGUI, KC_TAB,  LT(_AD, KC_BSPC), KC_LSFT,  KC_LCTL, KC_LALT, KC_SPC,  MO(_RS), KC_MINS, KC_QUOT, KC_ENT
+    KC_ESC, KC_LGUI, KC_TAB,  LT(_AD, KC_BSPC), KC_LSFT,  KC_LCTL, KC_LALT, KC_SPC,  MO(_RS), KC_MINS, KC_SLSH, KC_ENT
   ),
 
   /*
