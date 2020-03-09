034f8a134a085c312baa826123b1fc17d31d4310
James Hagerman <james.hagerman@gmail.com>
2020-03-09 14:32:52 -0700

    Reviung39 - Addiing a bunch of changes to get this keyboard closer to my atreus layout. Adding notes about the changes I made and what I would like to do next.

--- keyboards/reviung39/keymaps/jamis/keymap.c
+++ keyboards/reviung39/keymaps/jamis/keymap.c
@@ -17,15 +17,12 @@
 
 enum layer_names {
     _BASE,
-    _LOWER,
     _RAISE,
-    _ADJUST,
-    _I3
+    _LOWER,
+    _BAR,
+    _I3,
+    _ADJUST
 };
-
-#define LOWER  MO(_LOWER)
-#define RAISE  MO(_RAISE)
-#define ADJUST MO(_ADJUST)
 
 // Custom Key names
 // These are for use with my i3wm config to allow me to move quickly between screens and panels
@@ -73,22 +70,27 @@ enum layer_names {
 // DONE - Move Function keys to adjust layer; allow for linux term switching
 // DONE - Move arrow keys to Raise and Lower
 // DONE - Move special characters to match Atreus
-// Add i3 New Terminal
-// Add i3 New Browser
+// DONE - Add i3 New Terminal
+// DONE - Add i3 New Browser
+// DONE - Make BAR layer for shifting ONLY the modifiers to allow easier shortcuts
+// DONE - BAR layer bound to "Both layer keys" instead of ADJUST
+// DONE - Move ADJUST layer toggle to I3 layer
+// - make LOWER + t do something useful
 //
 // Consider adding:
 // Layers for "First Lower then Raise" and "First Raise then Lower"
-// LED Control for different layers!!!
-// LEDs failed! - Fix the Top-facing LED so it can be used for indicating which layer
+// LED Control for different layers
+//   This won't happen until I fix all the LEDs...
+//   Fix the Top-facing LED so it can be used for indicating which layer
 //
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_reviung39(
-    LT(_I3, KC_TAB), KC_Q,     KC_W,     KC_E,     KC_R,      KC_T,               KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC ,
-    KC_LCTL,         KC_A,     KC_S,     KC_D,     KC_F,      KC_G,               KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT ,
-    KC_LSFT,         KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,               KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_ENT  ,
-                                                 LT(LOWER, KC_BSPC),    KC_SPC,   RAISE
+    LT(_I3, KC_TAB),   KC_Q,     KC_W,     KC_E,     KC_R,      KC_T,               KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC ,
+    KC_LCTL,           KC_A,     KC_S,     KC_D,     KC_F,      KC_G,               KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT ,
+    LSFT_T(KC_ESC),    KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,               KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_ENT  ,
+                                                   LT(_LOWER, KC_BSPC),   KC_SPC,   MO(_RAISE)
   ),
-  
+ 
   [_RAISE] = LAYOUT_reviung39(
     KC_ESC,   KC_EXLM,  KC_AT,    KC_UP,    KC_LCBR,   KC_RCBR,            KC_PGUP,  KC_7,  KC_8,  KC_9,  KC_ASTR,  KC_DEL  ,
     _______,  KC_HASH,  KC_LEFT,  KC_DOWN,  KC_RGHT,   KC_MINS,            KC_PGDN,  KC_4,  KC_5,  KC_6,  KC_RBRC,  KC_BSLS ,
@@ -102,22 +104,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,  KC_LGUI,   KC_TAB,  KC_LALT,  KC_CAPS,   KC_QUOT,            KC_MPLY, KC_MRWD,    KC_MFFD,    WDEL_L,      WDEL_R,   _______ ,
                                                        _______,  KC_SPC,  _______
   ),
+ 
+  // update_tri_layer_state() doesn't work if the 3rd layer is lower than the first two :(
+  [_BAR] = LAYOUT_reviung39(
+    KC_LALT, KC_Q,     KC_W,     KC_E,     KC_R,      KC_T,               KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_BSPC ,
+    KC_LCTL, KC_A,     KC_S,     KC_D,     KC_F,      KC_G,               KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT ,
+    KC_LSFT, KC_Z,     KC_X,     KC_C,     KC_V,      KC_B,               KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_ENT  ,
+                                                     _______,   KC_ESC,   _______
+  ),
   
-  [_ADJUST] = LAYOUT_reviung39(
-    RGB_VAI,   RGB_SAI, RGB_HUI,  RGB_MOD,  XXXXXXX,   RGB_TOG,            XXXXXXX,  KC_F9,  KC_F10, KC_F11, KC_F12,  XXXXXXX,
-    RGB_VAD,   RGB_SAD, RGB_HUD,  RGB_RMOD, XXXXXXX,   XXXXXXX,            XXXXXXX,  KC_F5,  KC_F6,  KC_F7,  KC_F8,   XXXXXXX,
-    XXXXXXX,   XXXXXXX, XXXXXXX,  XXXXXXX,  XXXXXXX,   XXXXXXX,            RESET,    KC_F1,  KC_F2,  KC_F3,  KC_F4,   XXXXXXX,
-                                                       _______,  XXXXXXX,  _______
+  [_I3] = LAYOUT_reviung39(
+    XXXXXXX,   TERM_1,  TERM_2,   TERM_3,   TERM_4,   TERM_5,                  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
+    KC_LSFT,   TERM_X,  TERM_U,   TERM_Y,   TERM_P,   TERM_6,                  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  I3_BROW,
+    TERM_Y,    TERM_L,  TERM_D,   TERM_R,   TERM_C,   TERM_7,                  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  I3_TERM,
+                                                       _______,  TG(_ADJUST),  _______
   ),
 
-  [_I3] = LAYOUT_reviung39(
-    XXXXXXX,   TERM_1,  TERM_2,   TERM_3,   TERM_4,   TERM_5,            XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,
-    KC_LSFT,   TERM_X,  TERM_U,   TERM_Y,   TERM_P,   TERM_6,            XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  I3_BROW,
-    TERM_Y,    TERM_L,  TERM_D,   TERM_R,   TERM_C,   TERM_7,            XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  XXXXXXX,  I3_TERM,
-                                                       _______,  XXXXXXX,  _______
+  [_ADJUST] = LAYOUT_reviung39(
+    XXXXXXX,  RGB_VAI,   RGB_SAI, RGB_HUI,  RGB_MOD,   RGB_TOG,            XXXXXXX,  KC_F9,  KC_F10, KC_F11, KC_F12,  XXXXXXX,
+    XXXXXXX,  RGB_VAD,   RGB_SAD, RGB_HUD,  RGB_RMOD,  XXXXXXX,            XXXXXXX,  KC_F5,  KC_F6,  KC_F7,  KC_F8,   XXXXXXX,
+    XXXXXXX,  XXXXXXX,   XXXXXXX, XXXXXXX,  XXXXXXX,   XXXXXXX,            RESET,    KC_F1,  KC_F2,  KC_F3,  KC_F4,   XXXXXXX,
+                                                       _______,  TO(_BASE),  _______
   ),
+
 };
 
 layer_state_t layer_state_set_user(layer_state_t state) {
-  return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+  return update_tri_layer_state(state, _LOWER, _RAISE, _BAR);
 }
